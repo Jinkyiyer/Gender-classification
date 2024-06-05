def36ab75a24cffd16b6552ed69adc8bb8a0169c
Jinkyiyer <161192592+Jinkyiyer@users.noreply.github.com>
2024-06-05 22:08:30 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -90,6 +90,19 @@ Discover the power of artificial intelligence with this application, which lever
 """)
 
 
+# Set background image using an online URL
+page_bg_img = '''
+<style>
+  body {
+    background-image: url("https://raw.githubusercontent.com/Jinkyiyer/Gender-classification/main/ai%20background.jpg");
+    background-size: cover;
+  }
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
 
 # Example URL to hero image hosted on GitHub
 hero_img_url = "https://raw.githubusercontent.com/Jinkyiyer/Gender-classification/main/hero_img.jpg"
