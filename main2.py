ecec24657b2d1aebb0bafe99367b614dfa8ae3cd
Jinkyiyer <161192592+Jinkyiyer@users.noreply.github.com>
2024-06-06 10:15:58 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -205,7 +205,7 @@ st.markdown('<p class="custom-paragraph">Discover the power of artificial intell
 page_bg_img = '''
 <style>
   .stApp {
-    background-image: url("https://raw.githubusercontent.com/Jinkyiyer/Gender-classification/main/ai bk4.gif");
+    background-image: url("https://raw.githubusercontent.com/Jinkyiyer/Gender-classification/main/ai bk6.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: fixed;
